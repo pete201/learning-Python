6fa203e69edbad595f340844617679720991c25a
pete201 <peterdeeming@hotmail.com>
2022-01-28 10:07:22 +0000

    works well, file save commented out

--- Brain.py
+++ Brain.py
@@ -18,7 +18,7 @@ class Brain:
 
     # TODO - ERROR: I DO NEED OPTION '9' IN ALL BUT THE LAST SET OF PERMUTATIONS.  I.E. I NEED OPTION 9, BUT I DON'T NEED 9 LEVELS DEEP
 
-    ticTacToeGrid = ['1', '2', '3']  # small list for testing only
+    ticTacToeGrid = ['1', '2', '3', '4']  # small list for testing only
 
     # constructor: see if file exists, if not create a new empty brain
     def __init__(self, playername):
@@ -37,12 +37,13 @@ class Brain:
             print('Brain.init: building permDictionary')
             # create a new empty brain dictionary,
             self.permDictionary = self.GetPermutations()
-            print(self.permDictionary)
+            #print(self.permDictionary)
+            print('length of dictionary is:',sum(len(v) for v in self.permDictionary.values()))
             # store dictionary into filename
-            self.f = open(filename, 'w')
-            json.dump(self.permDictionary, self.f)
-            self.f.close()
-            print(f'Brain.init: file {filename} created')
+# self.f = open(filename, 'w')
+# json.dump(self.permDictionary, self.f)
+# self.f.close()
+# print(f'Brain.init: file {filename} created')
 
     def Permutate(self,stems, leaves):
         '''takes 2 strings; stems and leaves, and returns permutations by adding each leaf to each stem'''
@@ -70,19 +71,21 @@ class Brain:
         permutations = {}                   # this is the main dictionary that will be built up
         myStems = ['']                      # initial condition for stems
 
-        for move in Brain.ticTacToeGrid:
+        # NOTE 9 permutations results in hudreds of thousands of rows and slows down everything
+        # since the last move can only occupy one square anyway, i have chosen to do only 8 itterations
+        # which means that 'best_move' will have to be aware of this  
+        for n in range (len(Brain.ticTacToeGrid)-1):
             # a 'stem' is the set of move permutations so far.  e.g. [2,5] represents top middle, then centre tiles
-
-            #while len(myStems) <= len(Brain.ticTacToeGrid):
+            print(f'this is move {n}, len(stem)={len(myStems[0])}, len(Brain.ticTacToeGrid)={len(Brain.ticTacToeGrid)} and stems={myStems} \n')
             myStems = self.Permutate(myStems, Brain.ticTacToeGrid)
-            permutations[move] = dict.fromkeys(myStems, 0)
+            permutations[n] = dict.fromkeys(myStems, 0) # popluate each permutation with the initial value of 0
 
         return permutations
 
 
     # TODO destructor: make sure files are closed before exiting
-    # TODO get best_move from brain
-    # TODO learn from game_result - THIS EXISTS AS LearnMoves in main prog
+    # TODO get best_move from brain (only 1st 8 moves in game: no suggestion for last move since only 1 square left anyway)
+    # TODO learn from game_result - THIS EXISTS AS LearnMoves in main prog (if 9 moves in game, don't try to write to last move as it won't exist in dictionary)
 
 p1 = Brain('player1')
 print('done')
