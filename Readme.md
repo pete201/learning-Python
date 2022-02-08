698a194db354936fccd7bad33fb81f9d42040798
pete201 <peterdeeming@hotmail.com>
2022-02-08 21:24:10 +0000

    wroks, needs a tidy

--- Readme.md
+++ Readme.md
@@ -11,3 +11,12 @@ so the first ref is a game move integer, followed by tile ref strings...
     [0] - no moves have been made - show all possibilities
     [0][2] - no mvoes, show value of tile 2
     [2][2][3] - 2 moves made, first was '2', second was '3' - show all possibilities.
+
+recusive fn(dictionary):
+    for items in dictionary:
+        if dictionary.value is a dictionary
+            then newDictionary[thisKey] = recursive(dictionary.value)
+        else
+            newDictionary[thiskey] = dictioanry(from values)
+    
+    once finished for loop, return newDictionary
--- buildListDict.py
+++ buildListDict.py
@@ -1,73 +1,38 @@
 GRID = ['1', '2', '3', '4']
 
 
-#this works great for the inner dict
-# for key in GRID:
-#     myDict={}
-#     shortList = GRID[:]
-#     #shortList.remove(key)
-#     myDict[key] = dict.fromkeys(shortList,0)
-#     print('initial: ',myDict)
-
-
-
-def CreateDict(keysList, valsList):
-    '''from (list), extracts each vlaue and builds dictionary of remaining values'''
-    outerDict = {}
-    for k in keysList:
-        innerDict = {}
-        for key in valsList: 
-            shortList = valsList[:]
-            shortList.remove(key)
-            innerDict[key] = dict.fromkeys(shortList,0)  
-        outerDict[k] = innerDict # add innerDict to outerDict    
-    return (outerDict)
-
 def BuildNestedDict(input_dict):
-    #outerDict = {}
-    print('BuildNestedDict: input_dict:', input_dict)
-    innerDict = {}
-    for element in input_dict.values() :
-        print('BuildNestedDict: type(element)', type(element))
-        
-        if type(element) is dict:   # if it's a nested dictionary, then recursive call.  If not, then we work on input dictionary
-            print('BuildNestedDict: recursivne call----------------------------------------')
-            innerDict.update(BuildNestedDict(element))
-        # we get here is type NOT dictionary
-        valsList = list(input_dict.keys())   # create a list of the keys in innermost dictionary so far
-        print('BuildNestedDict: valsList:', valsList)
-        for key in valsList: 
-            shortList = valsList[:]
-            shortList.remove(key)
-            innerDict[key] = dict.fromkeys(shortList,0) 
-        print('BuildNestedDict: return inner:', innerDict)
-        return innerDict
+    returnDict = {}
+    #print('BuildNestedDict: input_dict:', input_dict)  
+    for input_key, input_value in input_dict.items():
+        #print('BuildNestedDict: type(element)', type(input_value))
+        if type(input_value) is dict:   # if it's a nested dictionary, then recursive call.  If not, then we work on input dictionary
+            #print('BuildNestedDict: recursivne call----------------------------------------')
+            returnValue = BuildNestedDict(input_value)
+            #print('BuildNestedDict: itteration return value:', returnValue)
+            #print('BuildNestedDict: and mykey:', input_key)
+            #innerDict['1'].update(BuildNestedDict(value))
+            returnDict[input_key] = returnValue
+            #print('BuildNestedDict: returnDict so far...', returnDict)
+        else:
+            # we get here if type NOT dictionary
+            valsList = list(input_dict.keys())   # create a list of the keys in innermost dictionary so far
+            #print('BuildNestedDict: valsList:', valsList)
+            for key in valsList: 
+                shortList = valsList[:]
+                shortList.remove(key)
+                returnDict[key] = dict.fromkeys(shortList,0) 
+
+    print('BuildNestedDict: returnDict:', returnDict)
+    return returnDict
 
 
 def main():
     ''''''
-    # # first establish List with initial values from Grid (i.e. the suggestion values for first move)
-    # ListDics = [dict.fromkeys(GRID,0)]
-    # myValslist = GRID[:]        # copy list 
-    # #mylist = list(ListDics[-1].keys())
-    # print(myValslist)
-    # myKeysList = [0]
-
-    # myDict = CreateDict(myKeysList, myValslist)
-    # print('myDict:', myDict)
-    # ListDics.append(myDict)
-    # print('ListDics:', ListDics)
-    # print('ListDics[0]:', ListDics[-1])
-    # print('ListDics[0].keys:', list(ListDics[-1].keys()))
-    # print('ListDics[-].values:', list(ListDics[-1].values()))
-
-    # # the 'list' i pass should be the list of keys from previous dictionary
-    # # so for first pass, this is simply 1,2,3,4 from keys from dictionary [0]
-    # # TODO lets start printing out the lists of keys...
-
+  
     # # first establish List with initial dictionary from Grid (i.e. the suggestion values for first move)
     ListDics = [dict.fromkeys(GRID,0)]
-    #print('main: ListDics:',ListDics)
+
     #now pass this dictionary to BuildNestedDictionary
     ListDics.append(BuildNestedDict(ListDics[0]))
     print('main: ListDics[0]:',ListDics)
